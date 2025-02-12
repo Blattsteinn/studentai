6148ec0357e3e17a97fa54ee851cff5975377a4e
Blattsteinn <arnas.lukosius@outlook.com>
2025-02-12 13:21:52 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,26 @@
 #include <iostream>
+#include <iostream>
+#include <vector>
+#include <iomanip>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+
+struct Studentas{
+
+    string vardas;
+    string pavarde;
+
+    vector<float> pazymiai;
+    int egzaminoRezultatas;
+
+    float galutinisVid;
+    float galutinisMed;
+};
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
