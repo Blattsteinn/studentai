da4b98fa4beb3932ebe00465622fae48d6b9a4c5
Blattsteinn <arnas.lukosius@outlook.com>
2025-02-12 13:22:42 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -23,6 +23,37 @@ struct Studentas{
 
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    
+    vector<Studentas> studentas;
+
+    int n, m; // n - pazymiu skaicius
+              // m - studentu skaicius
+    
+    m = 2; n = 3;
+
+    
+    Studentas laikinas;
+    for(int i=0; i < m; i++){
+
+        laikinas.pazymiai.clear();
+        float sum = 0;
+        
+        // cout << "Vardas: ";   cin >> laikinas.vardas;
+        // cout << "Pavarde: ";  cin >> laikinas.pavarde;
+
+//------------------------------- Pazymiu ivedimas --------------------------------------------------------------------------------------------
+        float ivertinimas;
+        for(int j=0; j < n; j++){
+            cout << j+1 << " pazymys: "; cin >> ivertinimas;
+            laikinas.pazymiai.push_back(ivertinimas);
+       }
+
+//------------------------------- Vidurkio skaiciavimas ----------------------------------------------------------------------------------------
+       for(auto s : laikinas.pazymiai){
+         sum += s;
+       } 
+       float vidurkis; //Pazymiu vidurkis
+       vidurkis = sum/n;
+
     return 0;
 }
