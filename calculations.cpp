072d7fe054af5333f932c7f1e0e3a931b1cde93b
Blattsteinn <arnas.lukosius@outlook.com>
2025-02-25 18:36:42 +0200

    Update calculations.cpp

--- calculations.cpp
+++ calculations.cpp
@@ -4,40 +4,36 @@ float average(Studentas laikinas){
     float sum = 0;
     float vidurkis = 0;
 
-    if(laikinas.pazymiai.size() != 0 ){ // Jei yra bent vienas ivertinimas apskaiciuojamas vidurki 
-        for(auto s : laikinas.pazymiai){
-            sum += s;
-        }   
-        vidurkis = sum/laikinas.pazymiai.size();
-        return vidurkis;
+    if(laikinas.pazymiai.size() == 0 ) {
+        throw std::runtime_error("[Klaida] pazymiu nera, tarpiniai rezultatai - 0.");
     }
-    else { 
-        cout << "[Pazymiu nera] namu darbu tarpiniai rezultatai = 0" << endl; 
-        return 0;   
-    }
-    
+
+    for(auto s : laikinas.pazymiai){
+        sum += s;
+    }   
+
+    vidurkis = sum/laikinas.pazymiai.size();
+    return vidurkis;
+
 }
 
 float median(Studentas laikinas){
 
     float mediana = 0; 
-        
-    if(laikinas.pazymiai.size() != 0 ){ // Jei yra bent vienas ivertinimas apskaiciuojama mediana
-        
-            sort(laikinas.pazymiai.begin(), laikinas.pazymiai.end()); // Duomenu issirikiavimas didejimo tvarka
-            int pazymiu_kiekis = laikinas.pazymiai.size();
+    if (laikinas.pazymiai.empty()) {
+        throw std::runtime_error("[Klaida] Negalima apskaiciuoti medianos, nera pazymiu.");
+    }
 
-            if(pazymiu_kiekis % 2 == 1){ // Jeigu pazymiu skaicius yra nelyginis
-                mediana = laikinas.pazymiai[pazymiu_kiekis / 2];
-                return mediana;
-            } 
+    sort(laikinas.pazymiai.begin(), laikinas.pazymiai.end()); // Duomenu issirikiavimas didejimo tvarka
+    int pazymiu_kiekis = laikinas.pazymiai.size();
 
-            else{ // Jeigu pazymiu skaicius yra lyginis
-                mediana = (laikinas.pazymiai[pazymiu_kiekis/2] + laikinas.pazymiai[pazymiu_kiekis/2 - 1 ]) / 2.0;}
-                return mediana;
-            } 
+    if(pazymiu_kiekis % 2 == 1){ // Jeigu pazymiu skaicius yra nelyginis
+        mediana = laikinas.pazymiai[pazymiu_kiekis / 2];
+        return mediana;
+    } 
 
-    else { 
-        return 0;   
+    else{ // Jeigu pazymiu skaicius yra lyginis
+        mediana = (laikinas.pazymiai[pazymiu_kiekis/2] + laikinas.pazymiai[pazymiu_kiekis/2 - 1 ]) / 2.0;
+        return mediana;
     }
 }
