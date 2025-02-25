1bb57f50546fa759363dcdea727833f5e11939c0
Blattsteinn <arnas.lukosius@outlook.com>
2025-02-25 17:00:21 +0200

    Update mano_lib.h

--- mano_lib.h
+++ mano_lib.h
@@ -40,8 +40,17 @@ struct Studentas{
     float galutinisMed;
 };
 
+// random_generating.cpp
+    int randomNumber(int a, int b);
+    vector<float> random_grade();
+    string random_name();
+    string random_last_name();
 
-// Prototipai
+//  calculations.cpp
+    float average(Studentas laikinas);
+    float median(Studentas laikinas);
+
+// fileRead.cpp
     string readFileToString();
     int wordCount(istringstream& iss);
     vector<Studentas> read_student_records(int ndCount, istringstream& iss);
